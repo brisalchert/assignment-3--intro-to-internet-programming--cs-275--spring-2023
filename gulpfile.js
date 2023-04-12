af6184fe1d45bd5dc497fda6dec6687b2121fd1e
brisalchert <brisalchert@gmail.com>
2023-04-12 14:23:09 -0400

    Stood up gulp

---
+++ gulpfile.js
@@ -0,0 +1,128 @@
+const { src, dest, series, watch } = require(`gulp`),
+    CSSLinter = require(`gulp-stylelint`),
+    cleanCSS = require(`gulp-clean-css`),
+    del = require(`del`),
+    babel = require(`gulp-babel`),
+    htmlCompressor = require(`gulp-htmlmin`),
+    jsCompressor = require(`gulp-uglify`),
+    jsLinter = require(`gulp-eslint`),
+    browserSync = require(`browser-sync`),
+    reload = browserSync.reload;
+
+let browserChoice = `default`;
+
+let lintJS = () => {
+    return src(`scripts/*.js`)
+        .pipe(jsLinter())
+        .pipe(jsLinter.formatEach(`compact`));
+};
+
+let lintCSS = () => {
+    return src(`styles/main.css`)
+        .pipe(CSSLinter({
+            failAfterError: false,
+            reporters: [
+                {formatter: `string`, console: true}
+            ]
+        }));
+};
+
+let transpileJSForDev = () => {
+    return src(`scripts/*.js`)
+        .pipe(babel())
+        .pipe(dest(`temp/js`));
+};
+
+let compressCSSForProd = () => {
+    return src(`styles/main.css`)
+        .pipe(cleanCSS({compatibility: `ie8`}))
+        .pipe(dest(`prod/styles`));
+};
+
+let compressHTML = () => {
+    return src(`index.html`)
+        .pipe(htmlCompressor({collapseWhitespace: true}))
+        .pipe(dest(`prod`));
+};
+
+let transpileJSForProd = () => {
+    return src(`scripts/*.js`)
+        .pipe(babel())
+        .pipe(jsCompressor())
+        .pipe(dest(`prod/scripts`));
+};
+
+let copyUnprocessedAssetsForDev = () => {
+    return src([
+        `styles*/*.css`,    // Source all .css,
+        `index.html`        // and index.html
+    ], {dot: true})
+        .pipe(dest(`temp`));
+};
+
+let serve = () => {
+    browserSync({
+        notify: true,
+        reloadDelay: 50,
+        browser: browserChoice,
+        server: {
+            baseDir: [
+                `temp`
+            ]
+        }
+    });
+
+    watch(`scripts/*.js`, series(lintJS, transpileJSForDev))
+        .on(`change`, reload);
+
+    watch(`styles/*.css`, lintCSS)
+        .on(`change`, reload);
+};
+
+async function clean() {
+    let fs = require(`fs`),
+        i,
+        foldersToDelete = [`./temp`, `prod`];
+
+    for (i = 0; i < foldersToDelete.length; i++) {
+        try {
+            fs.accessSync(foldersToDelete[i], fs.F_OK);
+            process.stdout.write(`\n\tThe ` + foldersToDelete[i] +
+                ` directory was found and will be deleted.\n`);
+            del(foldersToDelete[i]);
+        } catch (e) {
+            process.stdout.write(`\n\tThe ` + foldersToDelete[i] +
+                ` directory does NOT exist or is NOT accessible.\n`);
+        }
+    }
+
+    process.stdout.write(`\n`);
+}
+
+exports.lintJS = lintJS;
+exports.lintCSS = lintCSS;
+exports.transpileJSForDev = transpileJSForDev;
+exports.compressCSSForProd = compressCSSForProd;
+exports.compressHTML = compressHTML;
+exports.transpileJSForProd = transpileJSForProd;
+exports.copyUnprocessedAssetsForDev = copyUnprocessedAssetsForDev;
+exports.clean = clean;
+exports.default = series(
+    lintCSS,
+    lintJS,
+    transpileJSForDev,
+    copyUnprocessedAssetsForDev,
+    serve
+);
+exports.serve = series(
+    lintCSS,
+    lintJS,
+    transpileJSForDev,
+    copyUnprocessedAssetsForDev,
+    serve
+);
+exports.build = series(
+    compressHTML,
+    compressCSSForProd,
+    transpileJSForProd
+);
