da61f272bf7347a450fe7a4720b0836bc73e0048
Brian Salchert <brisalchert@gmail.com>
2023-04-15 03:27:05 -0400

    Added resize logic & side tray

--- scripts/main.js
+++ scripts/main.js
@@ -1,24 +1,78 @@
-const menu = document.getElementsByTagName(`nav`)[0];
+const nav = document.getElementsByTagName(`nav`)[0];
+const menu = nav.firstElementChild;
+const header = document.getElementsByTagName(`header`)[0];
 const menuButton = document.getElementById(`menu-button`);
 const modalButton = document.getElementById(`modal-button`);
+let breakpoint = 736;
 let menuDown = false;
-let menuTop = menu.clientTop;
+let sideTray = false;
+let navTop = nav.clientTop;
+let navLeft = nav.clientLeft;
 
 window.onload = () => {
-    console.log(menuTop);
+    if (window.innerWidth <= breakpoint) {
+        // Reset menu to side tray
+        nav.style.transition = `none`;
+        menu.style.display = `block`;
+        nav.style.transform = `translateY(0px)`;
+        nav.style.top = `${header.clientHeight}px`;
+        nav.style.left = `${(0 - menu.clientWidth)}px`;
+        nav.style.justifyContent = `left`;
+        menuDown = false;
+        sideTray = true;
+    }
+};
+
+window.onresize = () => {
+    if (!sideTray) {
+        if (window.innerWidth <= breakpoint) {
+            // Reset menu to side tray
+            nav.style.transition = `none`;
+            menu.style.display = `block`;
+            nav.style.transform = `translateY(0px)`;
+            nav.style.top = `${header.clientHeight}px`;
+            nav.style.left = `${(0 - menu.clientWidth)}px`;
+            nav.style.justifyContent = `left`;
+            menuDown = false;
+            sideTray = true;
+        }
+    }
+    else {
+        if (window.innerWidth > breakpoint) {
+            // Reset menu to drop down
+            nav.style.transition = `none`;
+            menu.style.display = `flex`;
+            nav.style.transform = `translateX(0px)`;
+            nav.style.top = `${(header.clientHeight - nav.clientHeight)}px`;
+            nav.style.left = `0`;
+            nav.style.justifyContent = `center`;
+            menuDown = false;
+            sideTray = false;
+        }
+    }
 };
 
 menuButton.addEventListener(`click`, () => {
-    if (menuDown) {
-        console.log(menuTop);
-        console.log(menu.clientHeight);
-        menu.style.transform = `translateY(${menuTop -= menu.clientHeight}px)`;
-        menuDown = false;
+    nav.style.transition = `500ms`;
+
+    if (window.innerWidth > breakpoint) {
+        if (menuDown) {
+            nav.style.transform = `translateY(${navTop -= nav.clientHeight}px)`;
+            menuDown = false;
+        }
+        else {
+            nav.style.transform = `translateY(${navTop = nav.clientHeight}px)`;
+            menuDown = true;
+        }
     }
     else {
-        console.log(menuTop);
-        console.log(menu.clientHeight);
-        menu.style.transform = `translateY(${menuTop += menu.clientHeight}px)`;
-        menuDown = true;
+        if (menuDown) {
+            nav.style.transform = `translateX(${navLeft -= menu.clientWidth}px)`;
+            menuDown = false;
+        }
+        else {
+            nav.style.transform = `translateX(${navLeft = menu.clientWidth}px)`;
+            menuDown = true;
+        }
     }
 });
